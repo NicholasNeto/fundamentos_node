8e632dc2f68a3dd581e5c41018bc2a31cac9da4e
NicholasNeto <nicholasneto@outlook.com>
2021-10-27 22:44:08 -0300

    change send for json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (request, response) => {
-    response.send('Hello Word !!')
+app.get("/", (request, response) => {
+    return response.json({ message: 'Hello, world Ignite' });
 })
 app.listen(3333)
