7febfde161a908820ab1e1a1dbe36ebfb47f4e4a
NicholasNeto <nicholasneto@outlook.com>
2021-10-28 08:57:43 -0300

    install nodemon in project

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 
 app.get("/", (request, response) => {
-    return response.json({ message: 'Hello, world Ignite' });
+    return response.json({ message: 'Hello, world Ignite - fundamentos de node.js' });
 })
 app.listen(3333)
